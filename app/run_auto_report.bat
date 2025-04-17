b3bf0c5c7101e36bebbad02a4741575ddc36d4ba
UllubiyZabitov <unzabitov@edu.hse.ru>
2025-04-17 22:25:09 +0300

    chore: убрано логирование из скриптов автозапуска для простоты

--- app/run_auto_report.bat
+++ app/run_auto_report.bat
@@ -2,31 +2,6 @@
 REM Переходим в корень проекта
 cd /d "%~dp0.."
 
-REM 1. Пробуем python из poetry
-for /f "delims=" %%i in ('poetry env info --path 2^>nul') do set VENV_PATH=%%i
-if exist "%VENV_PATH%\Scripts\python.exe" (
-    set "PYTHON_PATH=%VENV_PATH%\Scripts\python.exe"
-    echo Использую python из poetry: %PYTHON_PATH%
-) else (
-    set "PYTHON_PATH="
-)
+poetry run python app\auto_report.py
 
-REM 2. Пробуем python из .venv
-if not defined PYTHON_PATH if exist ".venv\Scripts\python.exe" (
-    set "PYTHON_PATH=.venv\Scripts\python.exe"
-    echo Использую python из .venv: %PYTHON_PATH%
-)
-
-REM 3. Пробуем системный python
-if not defined PYTHON_PATH (
-    where python >nul 2>nul && set "PYTHON_PATH=python" && echo Использую системный python
-)
-
-if not defined PYTHON_PATH (
-    echo Не удалось найти подходящий python!
-    pause
-    exit /b
-)
-
-REM Запускаем и логируем вывод
-"%PYTHON_PATH%" app\auto_report.py > app\run_auto_report.bat.log 2>&1 
+pause 
--- app/run_auto_report.sh
+++ app/run_auto_report.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
-PYTHON_PATH=".venv/bin/python"
-if [ ! -x "$PYTHON_PATH" ]; then
-  PYTHON_PATH="python3"
-fi
-"$PYTHON_PATH" app/auto_report.py
+
+poetry run python app/auto_report.py
